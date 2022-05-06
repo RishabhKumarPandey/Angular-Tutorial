1d9ce925f66f6d6a5f2dc6e83400f62b06903fcf
Rishabh Kumar <rishabhkumar3023@gmail.com>
2022-05-06 12:35:20 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { GlserviceService } from './glservice.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  userLoggedIn: boolean = false;
   title = 'Rishabh Kumar';
+  btnclick() {
+    prompt("Enter the first no.")
+  }
+  cars : Array <string> = ["BMW" ,"Audi", "Lexus", "Tesla"];
+  // constructor(){
+  //  console.log ("you are in component.tc")
+  // }
+  // @HostListener('click',['$event'])
+  // onhostclick(event:Event){
+  //   alert("The host is listening your event")
+  // }
 }
